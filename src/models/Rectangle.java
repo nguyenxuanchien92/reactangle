e28b727ad72a0a524c3aec1a1754d226fb25ed47
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-11-09 08:26:13 +0700

    done

---
+++ src/models/Rectangle.java
@@ -0,0 +1,39 @@
+package models;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getPerimeter(){
+        return (this.width + this.height) * 2;
+    }
+
+    public double getArea(){
+        return this.height * this.width;
+    }
+
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
